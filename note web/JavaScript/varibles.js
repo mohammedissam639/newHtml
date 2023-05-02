18bcbb8becee7fdc01404cd52d0d2fca0a548f89
mohammedissam639 <mohammedissam681@gmail.com>
2023-05-02 15:37:21 +0300

    add html files

---
+++ note web/JavaScript/varibles.js
@@ -0,0 +1,7 @@
+let rate = prompt("rate our website form 1 to 5");
+rate = parseInt(rate);
+let z = 8;
+for (let i=1;i<=rate;i++){
+    document.write("<img src = 'img\start.png' height='50' width='50'>")
+  
+}
